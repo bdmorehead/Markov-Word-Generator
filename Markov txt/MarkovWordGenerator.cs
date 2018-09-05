d971b194bce9b2de03321a48efd0617f78c58ddf
Bobby Morehead <bobby.morehead@outlook.com>
2018-09-05 09:06:24 -0400

    Creates structs for First, Middle, and Last char

---
+++ Markov txt/MarkovWordGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markov_txt
+{
+    struct FirstCharacter
+    {
+                            
+    }
+
+    struct MiddleCharacter
+    {
+
+    }
+
+    struct LastCharacter
+    {
+
+    }
+
+    class MarkovWordGenerator
+    {
+        FirstCharacter[] firstCharacters = new FirstCharacter[26];
+        MiddleCharacter[] middleCharacters = new MiddleCharacter[26];
+        LastCharacter[] lastCharacters = new LastCharacter[26];
+
+        public void Addword(string word)
+        {
+            if (word.Length > 3)
+            {
+                AddFirstCharacters();
+                AddMiddleCharacters();
+                AddEndCharacters();
+            }
+                    
+
+        }
+
+        private void AddFirstCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddMiddleCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddEndCharacters()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
